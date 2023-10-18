8531a77114dafd6c525ec91f8e5c5124d8539b76
Dom Webber <dom.webber@hotmail.com>
2023-10-18 10:16:35 +0100

    fix: Change enum from const enum to standard enum
    Closes #1

--- src/Message/MessageTypesEnum.ts
+++ src/Message/MessageTypesEnum.ts
@@ -6,7 +6,7 @@
  * @author Dom Webber <dom.webber@hotmail.com>
  * @see    https://greatdetail.com
  */
-const enum MessageTypesEnum {
+enum MessageTypesEnum {
   Audio = "audio", // for audio messages.
   Contacts = "contacts", // for contact messages.
   Document = "document", // for document messages.
