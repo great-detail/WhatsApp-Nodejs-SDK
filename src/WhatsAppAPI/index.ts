7789ae08cbad74db1eeaf52329cd32a299a8b98e
Dom Webber <dom.webber@hotmail.com>
2023-11-29 15:25:30 +0000

    docs: update JSDoc examples

--- src/WhatsAppAPI/index.ts
+++ src/WhatsAppAPI/index.ts
@@ -19,17 +19,6 @@ import type { Logger } from "winston";
  * @example
  * // SDK instantiation
  * const sdk = new WhatsAppAPI("123456")
- * @example
- * // Send a Text Message
- * const message = sdk.message.text({ body: "Hello"}, { toNumber: "1234567890" });
- * const sendReceipt = await message.send();
- * console.log(sendReceipt);
- * @example
- * // Receive a Message via Webhook
- * const app = express(); // or any other express-like app
- * app.get("/webhook", sdk.webhook.register);
- * app.post("/webhook", sdk.webhook.eventNotification);
- * app.listen(3000);
  */
 export default class WhatsAppAPI extends AbstractAPI {
   public message: MessageAPI;
