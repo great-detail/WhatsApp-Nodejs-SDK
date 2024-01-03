b93f0203e4988f3eb14262808f3581c60cd24c8d
Dom Webber <dom.webber@hotmail.com>
2024-01-03 17:14:13 +0000

    feat: introduce isUpgraded dynamic property on GraphResponse
    GraphResponse.isUpgraded indicates as to whether the request was upgraded to later (or simply different) API version on Facebook's end.

---
+++ .changeset/selfish-windows-buy.md
@@ -0,0 +1,5 @@
+---
+"@great-detail/whatsapp": patch
+---
+
+feat: introduce isUpgraded dynamic property on GraphResponse, indicative of API versioning upgrades
--- src/GraphResponse/index.ts
+++ src/GraphResponse/index.ts
@@ -6,6 +6,11 @@
  * @author Dom Webber <dom.webber@hotmail.com>
  * @see    https://greatdetail.com
  */
+import GraphRequest from "../GraphRequest/index.js";
+
+export interface GraphResponseParameters<T = unknown> extends ResponseInit {
+  request?: GraphRequest<T>;
+}
 
 /**
  * Graph API Response.
@@ -14,6 +19,33 @@
  * @author Dom Webber <dom.webber@hotmail.com>
  */
 export default class GraphResponse<T = unknown> extends Response {
+  public readonly request?: GraphRequest<T>;
+
+  constructor(
+    bodyInit: ReadableStream | null,
+    { request, ...parameters }: GraphResponseParameters<T> = {},
+  ) {
+    super(bodyInit, parameters);
+    this.request = request;
+  }
+
+  /**
+   * Whether the Request was upgraded in API versions.
+   * If the incoming version cannot be determined then undefined will be
+   * returned.
+   *
+   * @since 6.5.0
+   */
+  public get isUpgraded(): boolean | undefined {
+    const outgoingVersion = this.request?.version;
+    const incomingVersion = this.headers.get("facebook-api-version");
+    if (!incomingVersion || !outgoingVersion) {
+      return undefined;
+    }
+
+    return incomingVersion === outgoingVersion;
+  }
+
   public override json(): Promise<T> {
     return super.json();
   }
