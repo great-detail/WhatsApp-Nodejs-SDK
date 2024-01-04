7b43d1cbdb28d087464d476073c93df8f290406a
Dom Webber <dom.webber@hotmail.com>
2024-01-04 10:59:36 +0000

    test: introduce test cases for GraphRequest

---
+++ src/__tests__/GraphRequest.test.ts
@@ -0,0 +1,137 @@
+/**
+ * WhatsApp NodeJS SDK.
+ *
+ * @since  2.0.0
+ * @author Great Detail Ltd <info@greatdetail.com>
+ * @author Dom Webber <dom.webber@hotmail.com>
+ * @see    https://greatdetail.com
+ */
+import GraphRequest from "../GraphRequest/index.js";
+import { describe, test, expect, jest } from "@jest/globals";
+
+describe("GraphRequest", () => {
+  describe("Construction with varying parameters", () => {
+    test("When a forward slash is used as an endpoint, then the url should reflect that path", () => {
+      // Arrange
+      const endpoint = "/";
+
+      // Act
+      const graphRequest = new GraphRequest(endpoint);
+
+      // Assert
+      expect(graphRequest.url).toBe(
+        `https://graph.facebook.com/v18.0${endpoint}`,
+      );
+    });
+
+    test("When a standard path is used as an endpoint, then the url should reflect that path", () => {
+      // Arrange
+      const endpoint = "/example/path/to/something";
+
+      // Act
+      const graphRequest = new GraphRequest(endpoint);
+
+      // Assert
+      expect(graphRequest.url).toBe(
+        `https://graph.facebook.com/v18.0${endpoint}`,
+      );
+    });
+
+    test("When an alternative version is specified, then the url should reflect that version", () => {
+      // Arrange
+      const endpoint = "/path/to/something";
+      const version = "v0.0";
+
+      // Act
+      const graphRequest = new GraphRequest(endpoint, { version });
+
+      // Assert
+      expect(graphRequest.url).toBe(
+        `https://graph.facebook.com/${version}/path/to/something`,
+      );
+    });
+
+    test("When an alternative base url is specified, then the url should reflect that base url", () => {
+      // Arrange
+      const endpoint = "/path/to/something";
+      const baseUrl = "https://example.com";
+
+      // Act
+      const graphRequest = new GraphRequest(endpoint, { baseUrl });
+
+      // Assert
+      expect(graphRequest.url).toBe(`${baseUrl}/v18.0${endpoint}`);
+    });
+
+    test("When setting a header via the constructor, the header should be accessible as a parameter as expected", () => {
+      // Arrange
+      const endpoint = "/path/to/something";
+      const headerName = "Content-Type";
+      const headerValue = "application/json";
+
+      // Act
+      const graphRequest = new GraphRequest(endpoint, {
+        headers: { [headerName]: headerValue },
+      });
+
+      // Assert
+      expect(graphRequest.headers.get(headerName)).toBe(headerValue);
+    });
+  });
+
+  describe("Calling the send method", () => {
+    test("When the send method is called, then the response should be returned as expected", async () => {
+      // Arrange
+      const endpoint = "/path/to/something";
+      const graphRequest = new GraphRequest(endpoint);
+      const mockFetch = jest.fn<typeof fetch>();
+      const body = "exampleBody";
+      mockFetch.mockReturnValue(Promise.resolve(new Response(body)));
+
+      // Act
+      const response = await graphRequest.send({
+        fetch: mockFetch,
+      });
+      const responseText = await response.text();
+
+      // Assert
+      expect(mockFetch).toHaveBeenCalled();
+      expect(mockFetch).toHaveBeenCalledWith(graphRequest);
+      expect(responseText).toBe(body);
+      expect(response.request).toBe(graphRequest);
+    });
+
+    test("When send-time header overrides are set, then the headers should be merged as expected at send-time", async () => {
+      // Arrange
+      const endpoint = "/path/to/something";
+      const graphRequest = new GraphRequest(endpoint, {
+        headers: {
+          "x-will-merge": "willMerge",
+          "Content-Type": "willBeOverridden",
+        },
+      });
+      const mockFetch = jest.fn<typeof fetch>();
+      mockFetch.mockReturnValue(Promise.resolve(new Response()));
+
+      // Act
+      const response = await graphRequest.send({
+        fetch: mockFetch,
+        headers: {
+          "Content-Type": "application/xml",
+          "x-will-also-merge": "willAlsoMerge",
+        },
+      });
+
+      // Assert
+      expect(mockFetch).toHaveBeenCalled();
+      expect(response.request).toBe(graphRequest);
+      expect(response.request?.headers.get("Content-Type")).toBe(
+        "application/xml",
+      );
+      expect(response.request?.headers.get("x-will-merge")).toBe("willMerge");
+      expect(response.request?.headers.get("x-will-also-merge")).toBe(
+        "willAlsoMerge",
+      );
+    });
+  });
+});
