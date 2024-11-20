32f6e684799f361e30c5e475672c24e43276bf5e
Dom Webber <dom.webber@hotmail.com>
2024-11-20 17:01:12 +0000

    Remove message cli for future re-introduction

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,7 @@
 import { program } from "commander";
 import getStdin from "get-stdin";
 import { oraPromise } from "ora";
-import { CloudAPI } from "./index.js";
+import Client from "./index.js";
 import type { Options as OraOptions } from "ora";
 
 const oraOptions: OraOptions = {
@@ -20,7 +20,7 @@ const oraOptions: OraOptions = {
 const WHATSAPP_ACCESS_TOKEN = process.env.WHATSAPP_ACCESS_TOKEN;
 const WHATSAPP_PHONE_NUMBER_ID = process.env.WHATSAPP_PHONE_NUMBER_ID;
 
-const sdk = new CloudAPI();
+const sdk = new Client();
 program.name("whatsapp").description("WhatsApp SDK CLI");
 
 const mediaCommand = program.command("media").description("Media");
@@ -34,18 +34,14 @@ mediaCommand
     "WhatsApp Access Token",
     WHATSAPP_ACCESS_TOKEN,
   )
-  .action(async (mediaURL, options) => {
+  .action(async (mediaURL) => {
     const result = await oraPromise(
       () =>
-        sdk.media.download(mediaURL).send({
-          headers: {
-            Authorization: `Bearer ${options.accessToken}`,
-          },
-        }),
+        sdk.media.download(mediaURL),
       { ...oraOptions, text: "Downloading media" },
     );
 
-    const file = await result.response.arrayBuffer();
+    const file = await result.arrayBuffer();
     const fileBuffer = Buffer.from(file);
     process.stdout.write(fileBuffer);
   });
@@ -68,12 +64,7 @@ mediaCommand
     const result = await oraPromise(
       () =>
         sdk.media
-          .delete(mediaID, { phoneNumberID: options.phoneNumberId })
-          .send({
-            headers: {
-              Authorization: `Bearer ${options.accessToken}`,
-            },
-          }),
+          .delete({ mediaID, phoneNumberID: options.phoneNumberId }),
       { ...oraOptions, text: "Deleting media" },
     );
 
@@ -98,12 +89,7 @@ mediaCommand
     const result = await oraPromise(
       () =>
         sdk.media
-          .getURL(mediaID, { phoneNumberID: options.phoneNumberId })
-          .send({
-            headers: {
-              Authorization: `Bearer ${options.accessToken}`,
-            },
-          }),
+          .getURL({ mediaID, phoneNumberID: options.phoneNumberId }),
       { ...oraOptions, text: "Getting media URL" },
     );
 
@@ -132,95 +118,13 @@ mediaCommand
     const result = await oraPromise(
       () =>
         sdk.media
-          .upload(stdinBlob, {
+          .upload({
+            file: stdinBlob,
             phoneNumberID: options.phoneNumberId,
             mimeType: options.mimeType,
             filename: options.filename,
-          })
-          .send({
-            headers: {
-              Authorization: `Bearer ${options.accessToken}`,
-            },
           }),
       { ...oraOptions, text: "Uploading media" },
-    );
-
-    console.log(await result.json());
-  });
-
-const messageCommand = program.command("message").description("Message");
-const messageSendCommand = messageCommand.command("send");
-
-messageSendCommand
-  .command("image")
-  .description("Send a media message")
-  .argument("<TO_NUMBER>", "To Number")
-  .requiredOption(
-    "--phone-number-id <PHONE_NUMBER_ID>",
-    "From Phone Number ID",
-    WHATSAPP_PHONE_NUMBER_ID,
-  )
-  .requiredOption(
-    "--access-token <ACCESS_TOKEN>",
-    "WhatsApp Access Token",
-    WHATSAPP_ACCESS_TOKEN,
-  )
-  .requiredOption("--media-id <MEDIA_ID>", "Media ID")
-  .option("--filename <FILENAME>", "Filename")
-  .option("--caption <CAPTION>", "Caption")
-  .action(async (toNumber, options) => {
-    const result = await oraPromise(
-      () =>
-        sdk
-          .message({ phoneNumberID: options.phoneNumberId })
-          .image(
-            {
-              id: options.mediaId,
-              filename: options.filename,
-              caption: options.caption,
-            },
-            {
-              toNumber,
-            },
-          )
-          .send({
-            headers: {
-              Authorization: `Bearer ${options.accessToken}`,
-            },
-          }),
-      { ...oraOptions, text: "Sending image" },
-    );
-
-    console.log(await result.json());
-  });
-
-messageSendCommand
-  .command("text")
-  .description("Send a text message")
-  .argument("<TO_NUMBER>", "To Number")
-  .requiredOption(
-    "--phone-number-id <PHONE_NUMBER_ID>",
-    "From Phone Number ID",
-    WHATSAPP_PHONE_NUMBER_ID,
-  )
-  .requiredOption("--body <MESSAGE_TEXT_BODY>", "Message Body")
-  .requiredOption(
-    "--access-token <ACCESS_TOKEN>",
-    "WhatsApp Access Token",
-    WHATSAPP_ACCESS_TOKEN,
-  )
-  .action(async (toNumber, options) => {
-    const result = await oraPromise(
-      () =>
-        sdk
-          .message({ phoneNumberID: options.phoneNumberId })
-          .text({ body: options.body }, { toNumber })
-          .send({
-            headers: {
-              Authorization: `Bearer ${options.accessToken}`,
-            },
-          }),
-      { ...oraOptions, text: "Sending text message" },
     );
 
     console.log(await result.json());
