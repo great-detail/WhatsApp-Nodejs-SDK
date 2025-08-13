b9f970575ddcdace87dcfaa38801e80a8246a935
Dom Webber <dom.webber@hotmail.com>
2025-08-13 10:02:53 +0100

    Add more fun fields

--- src/types/BusinessAccount/index.ts
+++ src/types/BusinessAccount/index.ts
@@ -41,6 +41,7 @@ export type BusinessAccountFields =
   | "health_status"
   | "is_enabled_for_insights"
   | "marketing_messages_lite_api_status"
+  | "marketing_messages_onboarding_status"
   | "ownership_type"
   | "status";
 
@@ -48,13 +49,13 @@ export type BusinessAccount = {
   id: BusinessAccountID;
   name: string;
   account_review_status: string;
-  message_template_namespace: string;
+  message_template_namespace?: string;
   timezone_id: string;
   owner_business_info: {
     id: string;
     name: string;
   };
-  on_behalf_of_business_info: {
+  on_behalf_of_business_info?: {
     id: string;
     name: string;
     status: string;
@@ -64,9 +65,26 @@ export type BusinessAccount = {
   country?: string;
   currency: string;
   is_enabled_for_insights: boolean;
-  marketing_messages_lite_api_status: "INELIGIBLE" | unknown;
+  is_shared_with_partners?: boolean;
+  primary_funding_id?: string;
+  purchase_order_number?: string;
+  marketing_messages_lite_api_status:
+    | "INELIGIBLE"
+    | "ELIGIBLE"
+    | "ONBOARDED"
+    | "UNKNOWN";
+  marketing_messages_onboarding_status:
+    | "INELIGIBLE_ON_BEHALF_OF_WABA"
+    | "INELIGIBLE_INACTIVE_OR_RESTRICTED"
+    | "INELIGIBLE_COUNTRY_NOT_SUPPORTED"
+    | "INELIGIBLE_USING_WHATSAPP_BUSINESS_APP"
+    | "ELIGIBLE"
+    | "PENDING_VALID_PAYMENT_METHOD"
+    | "PENDING_INTERNAL_SETUP"
+    | "ONBOARDED";
   ownership_type: "CLIENT_OWNED" | unknown;
   status: "ACTIVE";
+  // TODO: Add health_status
 };
 
 export type GetBusinessAccountOptions = {
