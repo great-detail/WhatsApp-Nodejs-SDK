5e8536b769eeea502070aa170d8ad84f19568960
Dom Webber <dom.webber@hotmail.com>
2025-08-13 08:55:41 +0100

    Refactor GetBusinessProfileFields to use BusinessProfileVertical type for vertical field

--- src/types/BusinessProfile/index.ts
+++ src/types/BusinessProfile/index.ts
@@ -8,15 +8,57 @@
 
 import { PhoneNumberID } from "../PhoneNumber.js";
 
-export type GetBusinessProfileFields = {
-  about?: boolean;
-  address?: boolean;
-  description?: boolean;
-  email?: boolean;
-  profile_picture_url?: boolean;
-  websites?: boolean;
-  vertical?: boolean;
+export type BusinessProfileFields =
+  | "about"
+  | "address"
+  | "description"
+  | "email"
+  | "profile_picture_url"
+  | "websites"
+  | "vertical";
+
+export const BUSINESS_PROFILE_VERTICALS = [
+  "UNDEFINED",
+  "AUTO",
+  "BEAUTY",
+  "APPAREL",
+  "EDU",
+  "ENTERTAIN",
+  "EVENT_PLAN",
+  "FINANCE",
+  "GROCERY",
+  "ONLINE_GAMBLING",
+  "PHYSICAL_GAMBLING",
+  "OTC_DRUGS",
+  "ALCOHOL",
+  "GOVT",
+  "HOTEL",
+  "HEALTH",
+  "NONPROFIT",
+  "PROF_SERVICES",
+  "RETAIL",
+  "TRAVEL",
+  "RESTAURANT",
+  "NOT_A_BIZ",
+  "OTHER",
+] as const;
+export type BusinessProfileVertical =
+  (typeof BUSINESS_PROFILE_VERTICALS)[number];
+
+export type BusinessProfile = {
+  about?: string;
+  address?: string;
+  description?: string;
+  email?: string;
+  messaging_product: "whatsapp";
+  profile_picture_url?: string;
+  vertical: BusinessProfileVertical;
+  websites?: [string] | [string, string] | (string[] & NonNullable<unknown>);
 };
+
+export type GetBusinessProfileFields = Partial<
+  Record<BusinessProfileFields, boolean>
+>;
 
 export type GetBusinessProfileOptions<
   Fields extends GetBusinessProfileFields = object,
@@ -25,27 +67,8 @@ export type GetBusinessProfileOptions<
   fields?: Fields;
 };
 
-export type GetBusinessProfilePayload<
-  Fields extends GetBusinessProfileFields = object,
-> = {
-  data: [
-    {
-      about: Fields extends { about: true } ? string : undefined;
-      address: Fields extends { address: true } ? string : undefined;
-      description: Fields extends { description: true } ? string : undefined;
-      email: Fields extends { email: true } ? string : undefined;
-      messaging_product: "whatsapp";
-      profile_picture_url: Fields extends { profile_picture_url: true }
-        ? string
-        : undefined;
-      vertical: Fields extends { vertical: true }
-        ? "" | (string & NonNullable<unknown>)
-        : undefined;
-      websites: Fields extends { websites: true }
-        ? [string] | [string, string] | (string[] & NonNullable<unknown>)
-        : undefined;
-    },
-  ];
+export type GetBusinessProfilePayload = {
+  data: [BusinessProfile];
 };
 
 export type UpdateBusinessProfileOptions = {
